3f51c048e932b3c84c384b865e7c39d08238f4f3
wonderwall-sangjinhan <sangjin.han@wonderwall.kr>
2021-08-28 14:26:30 +0900

    ADD - prisma get Query

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,23 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { ApiResponse } from '@nestjs/swagger';
 
+import { User } from '.prisma/client';
+import { AppService } from './app.service';
+import { PrismaService } from './prisma/prisma.service';
 @Controller()
 export class AppController {
-    constructor(private readonly appService: AppService) {}
+    constructor(
+        private readonly appService: AppService,
+        private prisma: PrismaService
+    ) {}
 
     @Get()
     getHello(): string {
         return this.appService.getHello();
+    }
+
+    @Get('users')
+    findUsers() {
+        return this.prisma.user.findMany();
     }
 }
